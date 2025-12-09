a84d6111eaed08e48e81060c8e5d584c3e091a3b
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-12-09 10:23:31 +0100

    Create DummyDatabaseOnHitPoints.hlsl

---
+++ outofdatabaseFolder/DummyDatabaseOnHitPoints.hlsl
@@ -0,0 +1,5 @@
+//if theere is at least one theory in physics or maths that suit the need of a hypercube database, then, wwe can set functions
+//thaat differs from low dimensional analysises. So lets add a high degree tresholld for hit points on scattering. This could be helpfull for 
+//feynamn motives if the locatio   of the hit points matter. But to enhance this reshold, we can add a complex and topological function
+//according to lemmas in physics that help taking into account a new set of points when we deal with high degree scattering.
+//so the best will be to delete cihers. We can do it if we sample the holomorphic databases.
